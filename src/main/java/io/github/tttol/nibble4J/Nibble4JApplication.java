e01e4d25f85d87eb7a9475d4f077bf541a78abf6
t-takahashi <tttol.t4k.b1z@gmail.com>
2023-06-26 21:45:43 +0900

    initial commit

---
+++ src/main/java/io/github/tttol/nibble4J/Nibble4JApplication.java
@@ -0,0 +1,13 @@
+package io.github.tttol.nibble4J;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nibble4JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nibble4JApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tttol/nibble4J/Nibble4JApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tttol.nibble4J;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nibble4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
