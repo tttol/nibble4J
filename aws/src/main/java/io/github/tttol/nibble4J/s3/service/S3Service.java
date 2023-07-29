48eaf2a618e56db6254e42c29400b51adf2a5c77
TOL <tttol.t4k.b1z@gmail.com>
2023-07-30 08:33:33 +0900

    AWS SDK Java V2を使ってS3と接続する (#2)
    * add s3 sdk logic
    
    * create S3ClientSetup

--- aws/src/main/java/io/github/tttol/nibble4J/s3/service/S3Service.java
+++ aws/src/main/java/io/github/tttol/nibble4J/s3/service/S3Service.java
@@ -1,9 +1,40 @@
 package io.github.tttol.nibble4J.s3.service;
 
+import io.github.tttol.nibble4J.s3.client.S3ClientSetup;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class S3Service {
+    @Value("${app.aws.s3.bucketName}")
+    private String bucketName;
+
+    private final S3ClientSetup s3ClientSetup;
+
+    public String listObjects() {
+        final var s3Client = s3ClientSetup.build();
+        try (s3Client) {
+            final ListObjectsRequest listObjects = ListObjectsRequest
+                    .builder()
+                    .bucket(bucketName)
+                    .build();
+            final ListObjectsResponse res = s3Client.listObjects(listObjects);
+
+            final var objects = res.contents();
+            return objects.stream().map(S3Object::key).collect(Collectors.joining(", "));
+        } catch (final S3Exception e) {
+            log.error("S3 list objects error. ", e);
+            throw new RuntimeException(e);
+        }
+    }
 }
