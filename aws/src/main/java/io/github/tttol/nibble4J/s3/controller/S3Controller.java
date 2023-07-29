50b8d4a2c49d9fd0993075037df76456f901dbec
t-takahashi <tttol.t4k.b1z@gmail.com>
2023-07-29 23:05:31 +0900

    add s3 sdk logic

--- aws/src/main/java/io/github/tttol/nibble4J/s3/controller/S3Controller.java
+++ aws/src/main/java/io/github/tttol/nibble4J/s3/controller/S3Controller.java
@@ -1,14 +1,19 @@
 package io.github.tttol.nibble4J.s3.controller;
 
+import io.github.tttol.nibble4J.s3.service.S3Service;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("s3")
+@RequiredArgsConstructor
 public class S3Controller {
-    @GetMapping
-    public String demo() {
-        return "S3 demo";
+    private final S3Service s3Service;
+
+    @GetMapping("ls")
+    public String ls() {
+        return s3Service.listObjects();
     }
 }
--- aws/src/main/java/io/github/tttol/nibble4J/s3/service/S3Service.java
+++ aws/src/main/java/io/github/tttol/nibble4J/s3/service/S3Service.java
@@ -1,9 +1,50 @@
 package io.github.tttol.nibble4J.s3.service;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
+@Slf4j
 public class S3Service {
+    @Value("${app.aws.accessKey}")
+    private String accessKey;
+    @Value("${app.aws.accessSecret}")
+    private String accessSecret;
+    @Value("${app.aws.s3.bucketName}")
+    private String bucketName;
+
+    public String listObjects() {
+        System.setProperty("aws.accessKeyId", accessKey);
+        System.setProperty("aws.secretKey", accessSecret);
+
+        final var credentialsProvider = ProfileCredentialsProvider.create();
+        final var region = Region.AP_NORTHEAST_1;
+        final var s3Client = S3Client.builder()
+                .region(region)
+                .credentialsProvider(credentialsProvider)
+                .build();
+        try (s3Client) {
+            final ListObjectsRequest listObjects = ListObjectsRequest
+                    .builder()
+                    .bucket(bucketName)
+                    .build();
+            final ListObjectsResponse res = s3Client.listObjects(listObjects);
+
+            final var objects = res.contents();
+            return objects.stream().map(S3Object::key).collect(Collectors.joining(", "));
+        } catch (final S3Exception e) {
+            log.error("S3 list objects error. ", e);
+            throw new RuntimeException(e);
+        }
+    }
 }
